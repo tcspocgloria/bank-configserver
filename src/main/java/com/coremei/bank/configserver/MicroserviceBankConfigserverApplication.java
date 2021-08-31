0c3f878c4a90738d6e2d2b6d8db9f7e0df2b4995
TCS POC Gloria <tcspocgloria@gmail.com>
2021-08-31 15:10:56 -0500

    first commit

---
+++ src/main/java/com/coremei/bank/configserver/MicroserviceBankConfigserverApplication.java
@@ -0,0 +1,15 @@
+package com.coremei.bank.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MicroserviceBankConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceBankConfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coremei/bank/configserver/MicroserviceBankConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coremei.bank.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBankConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
